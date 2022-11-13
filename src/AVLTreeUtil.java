9e38ee24ca0afde2d8861133a4bb9879dddb0739
pypalkar23 <7241388+pypalkar23@users.noreply.github.com>
2022-11-12 21:56:01 -0500

    Minor refactor

--- src/AVLTreeUtil.java
+++ src/AVLTreeUtil.java
@@ -3,28 +3,16 @@ import java.util.List;
 
 //The Actual AVL Tree Class
 public class AVLTreeUtil {
-    //Individual Node Structure
-    class Node {
-        int data;
-        int height;
-        Node left;
-        Node right;
-
-        public Node(int data) {
-            this.height = 1;
-            this.data = data;
-        }
-    }
-
-    Node root;
+    //Individual TreeNode Structure
+    TreeNode root;
 
     //------------------Tree Print Block Start------------------
     //Used for debugging
-    private void inorder(Node node) {
-        if (node != null) {
-            inorder(node.left);
-            System.out.print(node.data+" ");
-            inorder(node.right);
+    private void inorder(TreeNode treeNode) {
+        if (treeNode != null) {
+            inorder(treeNode.left);
+            System.out.print(treeNode.data+" ");
+            inorder(treeNode.right);
         }
     }
 
@@ -35,167 +23,166 @@ public class AVLTreeUtil {
     //------------------Tree Print Block End------------------
 
     //------------------Insert Block Start------------------
-    public Node insert(int data) {
+    public TreeNode insert(int data) {
         this.root = insert(this.root, data);
         return this.root;
     }
 
-
-    private Node insert(Node node, int data) {
-        if (node == null) {
-            return (new Node(data));
+    private TreeNode insert(TreeNode treeNode, int data) {
+        if (treeNode == null) {
+            return (new TreeNode(data));
         }
         //insert to the left
-        else if (data < node.data)
-            node.left = insert(node.left, data);
+        else if (data < treeNode.data)
+            treeNode.left = insert(treeNode.left, data);
             //insert to the right
-        else if (data > node.data)
-            node.right = insert(node.right, data);
+        else if (data > treeNode.data)
+            treeNode.right = insert(treeNode.right, data);
         else
-            //if node is already present simply return that node
-            return node;
+            //if treeNode is already present simply return that treeNode
+            return treeNode;
 
-        //set height for ancestor node
-        node.height = 1+Math.max(getHeight(node.left),getHeight(node.right));
+        //set height for ancestor treeNode
+        treeNode.height = 1+Math.max(getHeight(treeNode.left),getHeight(treeNode.right));
 
         //get balance factor
-        int currentBF = getBalanceFactor(node);
+        int currentBF = getBalanceFactor(treeNode);
         /* RR Rotation */
-        if (currentBF > 1 && data < node.left.data)
-            return rotateRight(node);
+        if (currentBF > 1 && data < treeNode.left.data)
+            return rotateRight(treeNode);
 
         /* LL Rotation */
-        if (currentBF < -1 && data > node.right.data)
-            return rotateLeft(node);
+        if (currentBF < -1 && data > treeNode.right.data)
+            return rotateLeft(treeNode);
 
         /* LR Rotation */
-        if (currentBF > 1 && data > node.left.data) {
-            node.left = rotateLeft(node.left);
-            return rotateRight(node);
+        if (currentBF > 1 && data > treeNode.left.data) {
+            treeNode.left = rotateLeft(treeNode.left);
+            return rotateRight(treeNode);
         }
 
         /* RL Rotation */
-        if (currentBF < -1 && data < node.right.data) {
-            node.right = rotateRight(node.right);
-            return rotateLeft(node);
+        if (currentBF < -1 && data < treeNode.right.data) {
+            treeNode.right = rotateRight(treeNode.right);
+            return rotateLeft(treeNode);
         }
 
-        return node;
+        return treeNode;
     }
     //------------------Insert Block End------------------
     public void delete(int data) {
         this.root = delete(this.root, data);
     }
 
-    private Node delete(Node node, int data) {
-        if (node == null) {
-            return node;
+    private TreeNode delete(TreeNode treeNode, int data) {
+        if (treeNode == null) {
+            return treeNode;
         }
-        if (data < node.data) {
+        if (data < treeNode.data) {
             //lookup to the left for deletion
-            node.left = delete(node.left, data);
-        } else if (data > node.data) {
+            treeNode.left = delete(treeNode.left, data);
+        } else if (data > treeNode.data) {
             //lookup to the right for deletion
-            node.right = delete(node.right, data);
+            treeNode.right = delete(treeNode.right, data);
         } else {
-            //if one of the child is null replace the current node with non-null child
-            if ((node.left == null) || (node.right == null)) {
-                Node temp = null;
-                if (temp == node.left)
-                    temp = node.right;
+            //if one of the child is null replace the current treeNode with non-null child
+            if ((treeNode.left == null) || (treeNode.right == null)) {
+                TreeNode temp = null;
+                if (temp == treeNode.left)
+                    temp = treeNode.right;
                 else
-                    temp = node.left;
+                    temp = treeNode.left;
 
-                if (node == null) {
-                    node = null;
+                if (treeNode == null) {
+                    treeNode = null;
                 } else {
-                    node = temp;
+                    treeNode = temp;
                 }
 
             } else {
                     /*
-                    find predecessor to the current node
-                    replace current node's value with the predecessor's
+                    find predecessor to the current treeNode
+                    replace current treeNode's value with the predecessor's
                     and delete the predecessor
                     */
-                Node temp = this.findReplaceableNode(node.right);
-                node.data = temp.data;
-                node.right = delete(node.right, temp.data);
+                TreeNode temp = this.findReplaceableNode(treeNode.right);
+                treeNode.data = temp.data;
+                treeNode.right = delete(treeNode.right, temp.data);
             }
         }
 
-        if (node == null)
-            return node;
+        if (treeNode == null)
+            return treeNode;
 
         //set the height
-        node.height = Math.max(this.getHeight(node.left), this.getHeight(node.right)) + 1;
+        treeNode.height = Math.max(this.getHeight(treeNode.left), this.getHeight(treeNode.right)) + 1;
 
-        int balanceFactor = this.getBalanceFactor(node);
+        int balanceFactor = this.getBalanceFactor(treeNode);
 
         //LL Rotation
-        if (balanceFactor > 1 && getBalanceFactor(node.left) >= 0) {
-            return rotateRight(node);
+        if (balanceFactor > 1 && getBalanceFactor(treeNode.left) >= 0) {
+            return rotateRight(treeNode);
         }
         //LR Rotation
-        if (balanceFactor > 1 && getBalanceFactor(node.left) < 0) {
-            node.left = rotateLeft(node.left);
-            return rotateRight(node);
+        if (balanceFactor > 1 && getBalanceFactor(treeNode.left) < 0) {
+            treeNode.left = rotateLeft(treeNode.left);
+            return rotateRight(treeNode);
         }
         //RR Rotation
-        if (balanceFactor < -1 && getBalanceFactor(node.right) <= 0) {
-            return rotateLeft(node);
+        if (balanceFactor < -1 && getBalanceFactor(treeNode.right) <= 0) {
+            return rotateLeft(treeNode);
         }
         //LR Rotation
-        if (balanceFactor < -1 && getBalanceFactor(node.right) > 0) {
-            node.right = rotateRight(node.right);
-            return rotateLeft(node);
+        if (balanceFactor < -1 && getBalanceFactor(treeNode.right) > 0) {
+            treeNode.right = rotateRight(treeNode.right);
+            return rotateLeft(treeNode);
         }
 
-        return node;
+        return treeNode;
     }
 
     //AVL Helper function Block Start
-    private int getHeight(Node node) {
-        if (node == null)
+    private int getHeight(TreeNode treeNode) {
+        if (treeNode == null)
             return 0;
-        return node.height;
+        return treeNode.height;
     }
 
-    //calculates balance factor for the node in question
-    private int getBalanceFactor(Node node) {
-        if (node == null)
+    //calculates balance factor for the treeNode in question
+    private int getBalanceFactor(TreeNode treeNode) {
+        if (treeNode == null)
             return 0;
-        return this.getHeight(node.left) - this.getHeight(node.right);
+        return this.getHeight(treeNode.left) - this.getHeight(treeNode.right);
     }
 
-    //finds the node which is inorder successor of the node marked for deletion
-    private Node findReplaceableNode(Node node) {
-        Node current = node;
+    //finds the treeNode which is inorder successor of the treeNode marked for deletion
+    private TreeNode findReplaceableNode(TreeNode treeNode) {
+        TreeNode current = treeNode;
         while (current.left != null)
             current = current.left;
         return current;
     }
-    private Node rotateLeft(Node node) {
-        Node rightChild = node.right;
-        Node grandChild = rightChild.left;
+    private TreeNode rotateLeft(TreeNode treeNode) {
+        TreeNode rightChild = treeNode.right;
+        TreeNode grandChild = rightChild.left;
 
-        rightChild.left = node;
-        node.right = grandChild;
+        rightChild.left = treeNode;
+        treeNode.right = grandChild;
 
-        node.height = 1 + Math.max(this.getHeight(node.left), this.getHeight(node.right));
+        treeNode.height = 1 + Math.max(this.getHeight(treeNode.left), this.getHeight(treeNode.right));
         rightChild.height = 1 + Math.max(this.getHeight(rightChild.left), this.getHeight(rightChild.right));
 
         return rightChild;
     }
 
-    private Node rotateRight(Node node) {
-        Node leftChild = node.left;
-        Node grandChild = leftChild.right;
+    private TreeNode rotateRight(TreeNode treeNode) {
+        TreeNode leftChild = treeNode.left;
+        TreeNode grandChild = leftChild.right;
 
-        leftChild.right = node;
-        node.left = grandChild;
+        leftChild.right = treeNode;
+        treeNode.left = grandChild;
 
-        node.height = 1 + Math.max(this.getHeight(node.left), this.getHeight(node.right));
+        treeNode.height = 1 + Math.max(this.getHeight(treeNode.left), this.getHeight(treeNode.right));
         leftChild.height = 1 + Math.max(this.getHeight(leftChild.left), this.getHeight(leftChild.right));
 
         return leftChild;
@@ -208,38 +195,38 @@ public class AVLTreeUtil {
         return this.search(this.root, val);
     }
 
-    public String search(Node node, int val) {
-        if (node == null)
+    public String search(TreeNode treeNode, int val) {
+        if (treeNode == null)
             return null;
 
-        if (node.data == val)
-            return String.valueOf(node.data);
+        if (treeNode.data == val)
+            return String.valueOf(treeNode.data);
 
-        if (val < node.data)
-            return this.search(node.left, val);
+        if (val < treeNode.data)
+            return this.search(treeNode.left, val);
 
-        return this.search(node.right, val);
+        return this.search(treeNode.right, val);
     }
 
-    //Search for values in given range
+    //Search for values in given range and stores it in a list
     public List<String> search(int val1, int val2) {
         List<String> result = new LinkedList<>();
         this.search(this.root, val1, val2, result);
         return result;
     }
 
-    public void search(Node node, int val1, int val2, List<String> result) {
-        if (node == null)
+    public void search(TreeNode treeNode, int val1, int val2, List<String> result) {
+        if (treeNode == null)
             return;
 
-        if(val1<node.data){
-            search(node.left,val1,val2,result);
+        if(val1< treeNode.data){
+            search(treeNode.left,val1,val2,result);
         }
-        if (node.data >= val1 && node.data <= val2) {
-            result.add(String.valueOf(node.data));
+        if (treeNode.data >= val1 && treeNode.data <= val2) {
+            result.add(String.valueOf(treeNode.data));
         }
 
-        search(node.right,val1,val2,result);
+        search(treeNode.right,val1,val2,result);
     }
     //------------- search block end ------------
 
